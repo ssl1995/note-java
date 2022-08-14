f5a24e98c5e8fe8dc5913dfbeec48daac04245f2
songshenglin <872912679@qq.com>
2022-08-14 16:35:46 +0800

    初始化 note-base

---
+++ note-base/src/main/java/com/ssl/notebase/NoteBaseApplication.java
@@ -0,0 +1,13 @@
+package com.ssl.notebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteBaseApplication.class, args);
+    }
+
+}
---
+++ note-base/src/test/java/com/ssl/notebase/NoteBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssl.notebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
