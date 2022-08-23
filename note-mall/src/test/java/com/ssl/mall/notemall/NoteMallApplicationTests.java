57934d3d354da6407982f685c3634bd58b802ba3
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-08-23 17:38:34 +0800

    搭mybatis-generator

---
+++ note-mall/src/test/java/com/ssl/mall/notemall/NoteMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssl.mall.notemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
