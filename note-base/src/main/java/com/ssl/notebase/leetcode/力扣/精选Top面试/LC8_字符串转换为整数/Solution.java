fe6337284e83b6c8fe80a7ef8683d9094611a7a8
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-09-06 09:55:21 +0800

    字符串转换为整数

--- note-base/src/main/java/com/ssl/notebase/leetcode/力扣/精选Top面试/LC8_字符串转换为整数/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/leetcode/力扣/精选Top面试/LC8_字符串转换为整数/Solution.java
@@ -15,40 +15,36 @@ public class Solution {
      * 如果整数数超过 32 位有符号整数范围 [−231,  231 − 1] ，需要截断这个整数，使其保持在这个范围内。具体来说，小于 −231 的整数应该被固定为 −231 ，大于 231 − 1 的整数应该被固定为 231 − 1 。
      * 返回整数作为最终结果。
      */
-    public int myAtoi(String str) {
-        // 去掉首尾空格:原数组去首位空格后转换为字符数组
-        char[] c = str.trim().toCharArray();
-        if (c.length == 0) {
+    public int myAtoi(String s) {
+        // 0."   "需要trim
+        if (s == null || s.trim().length() == 0) {
             return 0;
         }
-        // num越界的两种情况:int型最大值21474836472147483647,
-        // 由于最后num=num*10+字符数,所以边界需要/10
-        int maxBoundary = Integer.MAX_VALUE / 10;
-        // sign:正负号标记,1正号,-1负号
-        int index = 0, sign = 1;
-        int num = 0;
-        // 第一个部分有三种情况:+/-/数字
-        if (c[0] == '-') {
+        char[] cs = s.trim().toCharArray();
+        // 1.处理正负号
+        // 默认正号
+        int sign = 1;
+        int index = 0;
+        if (cs[0] == '-') {
             sign = -1;
             index = 1;
-        } else if (c[0] == '+') {
+        } else if (cs[0] == '+') {
             index = 1;
         }
-        for (int i = index; i < c.length; i++) {
-            // 力扣：遇到非数字部分，可以忽略，直接当前得到的整数
-            if (c[i] < '0' || c[i] > '9') {
+        // 2.遍历
+        int num = 0;
+        for (int i = index; i < cs.length; i++) {
+            if (cs[i] < '0' || cs[i] > '9') {
                 break;
             }
-            // 1.num=最大值/10 且 c[i]>'7',乘积后必越界
-            // 2.num>最大值/10 乘积后必越界
-            if (num > maxBoundary || (num == maxBoundary && c[i] > '7')) {
-                // 越界后，返回整型最值
+            // 3.是否越界，超过最大整型
+            int temp = cs[i] - '0';
+            if (num > Integer.MAX_VALUE / 10 || (num == Integer.MAX_VALUE / 10 && temp > Integer.MAX_VALUE % 10)) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            // 不越界，才拼接数字部分
-            num = num * 10 + (c[i] - '0');
+            num = num * 10 + temp;
         }
-        // 返回符号*数字
+
         return sign * num;
     }
 }
