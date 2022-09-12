35c247e7c9e3333ee784c42f3b70f82f9b8ff90d
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-09-12 18:40:41 +0800

    加减法

--- note-base/src/main/java/com/ssl/notebase/leetcode/力扣/精选Top面试/LC29_两数相除/加减法/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/leetcode/力扣/精选Top面试/LC29_两数相除/加减法/Solution.java
@@ -44,6 +44,58 @@ public class Solution {
         return res;
     }
 
+    // 除法
+    public int divide(int dividend, int divisor) {
+
+        long a = dividend;
+        long b = divisor;
+
+        int flag = 1;
+        if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+            flag = -1;
+        }
+        if (a < 0) {
+            a = -a;
+        }
+        if (b < 0) {
+            b = -b;
+        }
+
+        // 两个数为正，除的结果在[0,a]内
+        long left = 0, right = a;
+        while (left < right) {
+            // 不能直接用/2;因为left = right时，就死循环
+//            long mid = (left + right) / 2;
+            // (..+1)/2,规避相同的情况
+            long mid = (left + right + 1) >> 1;
+            // 一个乘法
+            if (multi(mid, b) <= a) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        long res = flag == -1 ? -right : right;
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        return (int) res;
+    }
+
+    private long multi(long a, long b) {
+        long res = 0;
+        while (b != 0) {
+            if (((b & 1) == 1)) {
+                res += a;
+            }
+//            a += a;
+            a <<= 1;
+            b >>= 1;
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         int n = 10;
         System.out.println("n:" + n);
@@ -56,35 +108,6 @@ public class Solution {
         int a = 2;
         int b = 3;
         System.out.println(multi(a, b));
-    }
-
-    int INF = Integer.MAX_VALUE;
-    public int divide(int _a, int _b) {
-        long a = _a, b = _b;
-        boolean flag = false;
-        if ((a < 0 && b > 0) || (a > 0 && b < 0)) flag = true;
-        if (a < 0) a = -a;
-        if (b < 0) b = -b;
-
-
-        long l = 0, r = a;
-        while (l < r) {
-            long mid = l + r + 1 >> 1;
-            if (mul(mid, b) <= a) l = mid;
-            else r = mid - 1;
-        }
-        r = flag ? -r : r;
-        if (r > INF || r < -INF - 1) return INF;
-        return (int)r;
-    }
-    long mul(long a, long k) {
-        long ans = 0;
-        while (k > 0) {
-            if ((k & 1) == 1) ans += a;
-            k >>= 1;
-            a += a;
-        }
-        return ans;
     }
 
 
