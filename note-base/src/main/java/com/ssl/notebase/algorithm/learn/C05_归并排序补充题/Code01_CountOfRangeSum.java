ce300addaffe4b8fd95116e1082b7159fd41ed4b
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-09-22 20:04:12 +0800

    归并排序题补充题

--- note-base/src/main/java/com/ssl/notebase/algorithm/learn/C05_归并排序补充题/Code01_CountOfRangeSum.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/learn/C05_归并排序补充题/Code01_CountOfRangeSum.java
@@ -16,6 +16,7 @@ public class Code01_CountOfRangeSum {
         if (nums == null || nums.length == 0) {
             return 0;
         }
+        // 前缀和数组：便于获取arr[i,j]的累加和
         long[] sum = new long[nums.length];
         sum[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
@@ -29,22 +30,25 @@ public class Code01_CountOfRangeSum {
             return sum[L] >= lower && sum[L] <= upper ? 1 : 0;
         }
         int M = L + ((R - L) >> 1);
-        return process(sum, L, M, lower, upper) + process(sum, M + 1, R, lower, upper)
+        return process(sum, L, M, lower, upper)
+                + process(sum, M + 1, R, lower, upper)
                 + merge(sum, L, M, R, lower, upper);
     }
 
-    public static int merge(long[] arr, int L, int M, int R, int lower, int upper) {
+    public static int merge(long[] preSum, int L, int M, int R, int lower, int upper) {
         int ans = 0;
         int windowL = L;
         int windowR = L;
         // [windowL, windowR)
         for (int i = M + 1; i <= R; i++) {
-            long min = arr[i] - upper;
-            long max = arr[i] - lower;
-            while (windowR <= M && arr[windowR] <= max) {
+            // 已知preSum[i]和[lower,upper]
+            // 可以推算出preSum[0..i-1]有多少在[lower-preSum[i],upper-preSum[i]]
+            long min = preSum[i] - upper;
+            long max = preSum[i] - lower;
+            while (windowR <= M && preSum[windowR] <= max) {
                 windowR++;
             }
-            while (windowL <= M && arr[windowL] < min) {
+            while (windowL <= M && preSum[windowL] < min) {
                 windowL++;
             }
             ans += windowR - windowL;
@@ -56,16 +60,16 @@ public class Code01_CountOfRangeSum {
         int p1 = L;
         int p2 = M + 1;
         while (p1 <= M && p2 <= R) {
-            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+            help[i++] = preSum[p1] <= preSum[p2] ? preSum[p1++] : preSum[p2++];
         }
         while (p1 <= M) {
-            help[i++] = arr[p1++];
+            help[i++] = preSum[p1++];
         }
         while (p2 <= R) {
-            help[i++] = arr[p2++];
+            help[i++] = preSum[p2++];
         }
         for (i = 0; i < help.length; i++) {
-            arr[L + i] = help[i];
+            preSum[L + i] = help[i];
         }
         return ans;
     }
--- note-base/src/main/java/com/ssl/notebase/interview/demo1/Main.java
+++ note-base/src/main/java/com/ssl/notebase/interview/demo1/Main.java
@@ -11,72 +11,29 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        sc.close();
-        int res = 0;
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = 1; j <= str.length() - i; j++) {
-                String sub = str.substring(i, i + j);
-//                System.out.println(sub);
-                list.add(sub);
-            }
-        }
-        for (int i = 0; i < list.size(); i++) {
-            if (checkGoodStr(list.get(i))) {
-                res++;
-            }
+        int n = 3;
+        for (int i = 0; i < 10; i++) {
+            // 测试一下
+            System.out.println(getNStr(n));
         }
 
-
-        System.out.println(res);
     }
 
     /**
-     * a
-     * ab
-     * aba
-     * abab
-     * ababa
-     * b
-     * ba
-     * bab
-     * baba
-     * a
-     * ab
-     * aba
-     * b
-     * ba
-     * a
-     * @param sub
-     * @return
+     * 输入一个n，返回一个指定n长度的01字符串
      */
-    private static boolean checkGoodStr(String sub) {
-        char[] cs = sub.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
-        for (char c : cs) {
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
-            } else {
-                map.put(c, 1);
-            }
+    private static String getNStr(int n) {
+        char[] subCs = new char[2];
+        subCs[0] = '0';
+        subCs[1] = '1';
+
+        char[] cs = new char[n];
+        for (int i = 0; i < n; i++) {
+            cs[i] = subCs[(int) ((1 + 1) * Math.random()) - (int) (1 * Math.random())];
         }
-        // 偶数
-        int cn1 = 0;
-        // 奇数
-        int cn2 = 0;
-        for (Integer value : map.values()) {
-            if (value % 2 == 0) {
-                cn1++;
-            } else {
-                cn2++;
-            }
-        }
-        if (cn2 != 1) {
-            return false;
-        }
-        return true;
+
+        return new String(cs);
     }
+
 
 }
