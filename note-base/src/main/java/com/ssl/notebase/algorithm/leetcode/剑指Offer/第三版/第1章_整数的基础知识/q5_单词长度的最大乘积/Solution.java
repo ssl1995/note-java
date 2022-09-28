593da7e1a282fd25814c49fed9412c05c2af9908
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-09-28 15:41:39 +0800

    单词长度的最大乘积

--- note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第1章_整数的基础知识/q5_单词长度的最大乘积/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第1章_整数的基础知识/q5_单词长度的最大乘积/Solution.java
@@ -5,6 +5,14 @@ package com.ssl.notebase.algorithm.leetcode.剑指Offer.第三版.第1章_整数
  * @date 2021/9/10
  */
 public class Solution {
+
+    /**
+     * 给定一个字符串数组words，请计算当两个字符串 words[i] 和 words[j] 不包含相同字符时，
+     * 它们长度的乘积的最大值。假设字符串中只包含英语的小写字母。如果没有不包含相同字符的一对字符串，返回 0。
+     * 输入: words = ["abcw","baz","foo","bar","fxyz","abcdef"]
+     * 输出: 16
+     * 解释: 这两个单词为 "abcw", "fxyz"。它们不包含相同字符，且长度的乘积最大。
+     */
     // map法：时间复杂度O(nk+n²)
     public int maxProduct(String[] words) {
         int N = 26;
