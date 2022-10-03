cc631820882ca017f4712a035988f3ae704a7b1d
songshenglin <872912679@qq.com>
2022-10-03 12:23:29 +0800

    二叉树递归套路

--- note-base/src/main/java/com/ssl/notebase/interview/demo1/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/interview/demo1/Solution.java
@@ -1,7 +1,7 @@
 package com.ssl.notebase.interview.demo1;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.ssl.notebase.algorithm.leetcode.utils.TreeNode;
+
 
 /**
  * @author SongShengLin
@@ -10,31 +10,66 @@ import java.util.List;
  */
 public class Solution {
 
-    public int maxProduct(String[] words) {
-        if (words == null || words.length == 0) {
-            return 0;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
         }
-        int[] flags = new int[words.length];
-        for (int i = 0; i < words.length; i++) {
-            for (char c : words[i].toCharArray()) {
-                flags[i] |= 1 << (c - 'a');
-            }
-        }
+        return process(root).isBST;
+    }
 
-        int max = 0;
-        for (int i = 0; i < words.length; i++) {
-            for (int j = i + 1; j < words.length; j++) {
-//                if (flags[i] != flags[j]) {
-//                    max = Math.max(max, words[i].length() * words[j].length());
-//                }
-                if ((flags[i] & flags[j]) == 0) {
-                    max = Math.max(max, words[i].length() * words[j].length());
-                }
-            }
+    public static class Info {
+        public boolean isBST;
+        // 每个结点的最大值和最小值都要
+        public int max;
+        public int min;
+
+        public Info(boolean i, int ma, int mi) {
+            isBST = i;
+            max = ma;
+            min = mi;
         }
 
+    }
 
-        return max;
+    public static Info process(TreeNode x) {
+        if (x == null) {
+            // 空树：不知道怎么设置，就返回null，在上游处理
+            return null;
+        }
+        // x左树、右树信息
+        Info leftInfo = process(x.left);
+        Info rightInfo = process(x.right);
+        // x的最大值
+        int max = x.val;
+        if (leftInfo != null) {
+            max = Math.max(max, leftInfo.max);
+        }
+        if (rightInfo != null) {
+            max = Math.max(max, rightInfo.max);
+        }
+        // x的最小值
+        int min = x.val;
+        if (leftInfo != null) {
+            min = Math.min(min, leftInfo.min);
+        }
+        if (rightInfo != null) {
+            min = Math.min(min, rightInfo.min);
+        }
+        // x的平衡性
+        boolean isBST = true;
+        if (leftInfo != null && !leftInfo.isBST) {
+            isBST = false;
+        }
+        if (rightInfo != null && !rightInfo.isBST) {
+            isBST = false;
+        }
+        if (leftInfo != null && leftInfo.max >= x.val) {
+            isBST = false;
+        }
+        if (rightInfo != null && rightInfo.min <= x.val) {
+            isBST = false;
+        }
+        return new Info(isBST, max, min);
     }
 
 
