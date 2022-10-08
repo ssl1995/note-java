19772f8b3504fc6aa65dd519694c984edbaacea9
songshenglin <872912679@qq.com>
2022-10-08 21:04:03 +0800

    RedisTemplate

--- note-redis/src/test/java/com/note/ssl/note/redis/NoteRedisApplicationTests.java
+++ note-redis/src/test/java/com/note/ssl/note/redis/NoteRedisApplicationTests.java
@@ -1,13 +1,29 @@
 package com.note.ssl.note.redis;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Objects;
 
 @SpringBootTest
 class NoteRedisApplicationTests {
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
-    void contextLoads() {
+    void initTest() {
+        String ping = Objects.requireNonNull(redisTemplate.getConnectionFactory()).getConnection().ping();
+        System.out.println("ping = " + ping);
+    }
+
+    @Test
+    void strTest() {
+        redisTemplate.opsForValue().set("username","张三1");
+        Object username = redisTemplate.opsForValue().get("username");
+        System.out.println((String)username);
     }
 
 }
