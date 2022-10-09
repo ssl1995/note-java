aa179d168b901c94f9c295d5fabcc5a4e1882fc1
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-10-09 09:07:37 +0800

    省份数量

--- note-base/src/main/java/com/ssl/notebase/algorithm/learn/C15_并查集/Code01_FriendCircles.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/learn/C15_并查集/Code01_FriendCircles.java
@@ -5,7 +5,7 @@ package com.ssl.notebase.algorithm.learn.C15_并查集;
 // 可以直接通过
 public class Code01_FriendCircles {
     /**
-     *
+     * LC547：省份数量
      */
     public static int findCircleNum(int[][] M) {
         int N = M.length;
@@ -24,23 +24,25 @@ public class Code01_FriendCircles {
     }
 
     /**
-     * 手写一个并查集
+     * 手写一个并查集：
+     * int实现
      */
     public static class UnionFind {
         // parent[i] = k ： i的父亲是k
         private int[] parent;
         // size[i] = k ： 如果i是代表节点，size[i]才有意义，否则无意义
-        // i所在的集合大小是多少
+        // 意思是i所在的集合大小是多少
         private int[] size;
-        // 辅助结构
-        private int[] help;
+        // 辅助结构，使用int做栈操作
+        private int[] stack;
         // 一共有多少个集合
         private int sets;
 
         public UnionFind(int N) {
             parent = new int[N];
             size = new int[N];
-            help = new int[N];
+            stack = new int[N];
+            // 初试化每个数字都是单独的省份
             sets = N;
             for (int i = 0; i < N; i++) {
                 parent[i] = i;
@@ -52,12 +54,16 @@ public class Code01_FriendCircles {
         // 这个过程要做路径压缩
         private int find(int i) {
             int hi = 0;
+            // 往上找父亲
             while (i != parent[i]) {
-                help[hi++] = i;
+                // 沿途经过的路径进栈
+                stack[hi++] = i;
+                // 找父亲
                 i = parent[i];
             }
+            // 出栈，路径压缩
             for (hi--; hi >= 0; hi--) {
-                parent[help[hi]] = i;
+                parent[stack[hi]] = i;
             }
             return i;
         }
@@ -66,6 +72,7 @@ public class Code01_FriendCircles {
             int f1 = find(i);
             int f2 = find(j);
             if (f1 != f2) {
+                // 小挂大
                 if (size[f1] >= size[f2]) {
                     size[f1] += size[f2];
                     parent[f2] = f1;
@@ -73,6 +80,7 @@ public class Code01_FriendCircles {
                     size[f2] += size[f1];
                     parent[f1] = f2;
                 }
+                // f1!=f2，每次都是独立的一个合并，sets--
                 sets--;
             }
         }
@@ -80,6 +88,19 @@ public class Code01_FriendCircles {
         public int sets() {
             return sets;
         }
+    }
+
+    public static void main(String[] args) {
+        int[][] nums = {
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}};
+
+        // parent:[0,0,2]
+        // size:[2,1,1]
+        // help:[0,0,0]
+        // sets:2
+        System.out.println(findCircleNum(nums));
     }
 
 }
