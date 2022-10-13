71da23d4f94e5e98cec39779277022795832b5be
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-10-13 15:30:57 +0800

    动态规划2

--- note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/编号刷题/LC1143_最长公共子序列/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/编号刷题/LC1143_最长公共子序列/Solution.java
@@ -6,23 +6,32 @@ public class Solution {
      * 输入：text1 = "abcde", text2 = "ade"输出：3
      * 解释：最长公共子序列是 "ade"，它的长度为 3。
      */
-    public int longestCommonSubsequence(String text1, String text2) {
-        int len1 = text1.length();
-        int len2 = text2.length();
-        // dp[i][j] 表示的是 text1[0…i-1] 和 text2[0…j-1] 的最长公共子序列的长度。
-        int[][] dp = new int[len1 + 1][len2 + 1];
-
-        for (int i = 1; i <= len1; i++) {
-            for (int j = 1; j <= len2; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
+    public int longestCommonSubsequence(String s1, String s2) {
+        if (s1 == null || s2 == null || s1.length() == 0 || s2.length() == 0) {
+            return 0;
+        }
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        int N = str1.length;
+        int M = str2.length;
+        int[][] dp = new int[N][M];
+        // i==j==0情况
+        dp[0][0] = str1[0] == str2[0] ? 1 : 0;
+        for (int j = 1; j < M; j++) {
+            dp[0][j] = str1[0] == str2[j] ? 1 : dp[0][j - 1];
+        }
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = str1[i] == str2[0] ? 1 : dp[i - 1][0];
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < M; j++) {
+                int p1 = dp[i - 1][j];
+                int p2 = dp[i][j - 1];
+                int p3 = str1[i] == str2[j] ? (1 + dp[i - 1][j - 1]) : 0;
+                dp[i][j] = Math.max(p1, Math.max(p2, p3));
             }
         }
-
-        return dp[len1][len2];
+        return dp[N - 1][M - 1];
     }
 
     public static void main(String[] args) {
