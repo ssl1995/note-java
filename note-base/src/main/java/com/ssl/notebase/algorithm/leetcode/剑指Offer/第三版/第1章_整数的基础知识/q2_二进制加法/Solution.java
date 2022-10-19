926a46130a9a672fbbb299a90f845467921850a3
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-10-19 17:46:01 +0800

    两个字符串二进制相加

--- note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第1章_整数的基础知识/q2_二进制加法/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第1章_整数的基础知识/q2_二进制加法/Solution.java
@@ -6,4 +6,26 @@ package com.ssl.notebase.algorithm.leetcode.剑指Offer.第三版.第1章_整数
  */
 public class Solution {
 
+    /**
+     * 两个字符串二进制相加
+     */
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int x = i >= 0 ? a.charAt(i--) - '0' : 0;
+            int y = j >= 0 ? b.charAt(j--) - '0' : 0;
+            int sum = x + y + carry;
+            carry = sum >= 2 ? 1 : 0;
+            sum = sum >= 2 ? sum - 2 : sum;
+            sb.append(sum);
+        }
+        if (carry == 1) {
+            sb.append(1);
+        }
+        // 返回结果反转
+        return sb.reverse().toString();
+    }
 }
