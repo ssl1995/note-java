6d59c8c2ee4a6818961efbcd6557c2871e6c43bc
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-10-26 09:37:42 +0800

    链表题

--- note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第4章_链表/q24_反转链表/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第4章_链表/q24_反转链表/Solution.java
@@ -8,16 +8,37 @@ import com.ssl.notebase.algorithm.leetcode.utils.ListNode;
  * @description 反转单向链表
  */
 public class Solution {
+    /**
+     * 反转单链表
+     */
     public ListNode reverseList(ListNode head) {
-        // pre = 待反转结点的前一个结点，最后返回它
+        if (head == null) {
+            return null;
+        }
         ListNode pre = null;
         ListNode cur = head;
+        ListNode next = null;
         while (cur != null) {
-            ListNode next = cur.next;
+            next = cur.next;
             cur.next = pre;
             pre = cur;
             cur = next;
         }
+
         return pre;
+    }
+
+    /**
+     * 反转单链表
+     * 递归法
+     */
+    public ListNode reverseList1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode ret = reverseList1(head.next);
+        head.next.next = head;
+        head.next = null;
+        return ret;
     }
 }
