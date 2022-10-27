1d608e8b34a921cf6d95520320375730e6c352ed
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-10-27 09:41:02 +0800

    重排链表

--- note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第4章_链表/q27_回文链表/Solution.java
+++ note-base/src/main/java/com/ssl/notebase/algorithm/leetcode/剑指Offer/第三版/第4章_链表/q27_回文链表/Solution.java
@@ -9,31 +9,54 @@ import com.ssl.notebase.algorithm.leetcode.utils.ListNode;
  */
 public class Solution {
 
+    /**
+     * 回文链表
+     */
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) {
+            // 注意：空节点或者单个节点是回文链表
             return true;
         }
-        // 快慢指针，找中间节点：奇数正中间，偶数中间前一个
-        ListNode slow = head;
-        ListNode fast = head.next;
+        // 1.找到中间节点：奇数返回中间，偶数返回前一个
+        ListNode mid = getMidNode(head);
+        // 2.反转后半部分链表
+        ListNode head2 = mid.next;
+        head2 = reverseList(head2);
+        // 3.前后2个链表值判断是否回文
+        return isPalindromeList(head, head2);
+    }
+
+
+    private ListNode getMidNode(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return head;
+        }
+        ListNode fast = head.next.next;
+        ListNode slow = head.next;
         while (fast.next != null && fast.next.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
-
-        // 找到后半部分链表的头结点
-        ListNode secondHead = slow.next;
-        // 如果链表总数是奇数，跳过中间节点，找slow的next.next作为后半部分的头结点
-        if (fast.next != null) {
-            secondHead = slow.next.next;
-        }
-        // 慢节点的后继置空，才能反转判断
-        slow.next = null;
-        return equals(secondHead, reverseList(head));
+        return slow;
     }
 
-    // 判断两个链表值是否相同
-    private Boolean equals(ListNode head1, ListNode head2) {
+    private ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode ret = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return ret;
+    }
+
+    private boolean isPalindromeList(ListNode head1, ListNode head2) {
+        if (head1 == null && head2 != null) {
+            return false;
+        }
+        if (head2 == null && head1 != null) {
+            return false;
+        }
         while (head1 != null && head2 != null) {
             if (head1.val != head2.val) {
                 return false;
@@ -41,20 +64,22 @@ public class Solution {
             head1 = head1.next;
             head2 = head2.next;
         }
-        return head1 == null && head2 == null;
+        return true;
     }
 
-    // 反转单链表
-    private ListNode reverseList(ListNode head) {
-        ListNode pre = null;
-        ListNode cur = head;
-        while (cur != null) {
-            ListNode next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return pre;
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(2);
+//        ListNode n4 = new ListNode(3);
+//        ListNode n5 = new ListNode(2);
+        n1.next = n2;
+        n2.next = n3;
+//        n3.next = n4;
+//        n4.next = n5;
+        Solution solution = new Solution();
+        System.out.println(solution.isPalindrome(n1));
+
     }
 
 }
