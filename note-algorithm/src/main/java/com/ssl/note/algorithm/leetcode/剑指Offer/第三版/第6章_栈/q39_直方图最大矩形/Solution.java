805cebee2300c439366b6aceee55f928ab36bfab
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-11-01 09:46:18 +0800

    直方图中最大矩形面积

--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第6章_栈/q39_直方图最大矩形/Solution.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第6章_栈/q39_直方图最大矩形/Solution.java
@@ -1,23 +1,43 @@
 package com.ssl.note.algorithm.leetcode.剑指Offer.第三版.第6章_栈.q39_直方图最大矩形;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+
 /**
  * @author SongShengLin
  * @date 2021/10/7 9:47 上午
  * @description
  */
 public class Solution {
-    // 暴力法:时间超时
+    /**
+     * 柱状图中最大的矩形
+     * 输入：heights = [2,1,5,6,2,3]
+     * 输出：10
+     * 解释：最大的矩形为图中红色区域，面积为 10
+     */
     public int largestRectangleArea(int[] heights) {
+        if (heights == null) {
+            return 0;
+        }
+        // 单调栈
+        Deque<Integer> stack = new LinkedList<>();
         int maxArea = Integer.MIN_VALUE;
-        // 依次找出最矮矩形，和其后的矩形面积比较找最小
         for (int i = 0; i < heights.length; i++) {
-            // min表示从i位置往后，能包含当前柱子的最小柱子高度
-            int min = heights[i];
-            for (int j = i; j < heights.length; j++) {
-                min = Math.min(min, heights[j]);
-                int area = min * (j - i + 1);
-                maxArea = Math.max(maxArea, area);
+            // 栈中保证单调递增，遇到待加入的小的，就要出栈
+            while (!stack.isEmpty() && heights[i] <= heights[stack.peek()]) {
+                int j = stack.pop();
+                int k = stack.isEmpty() ? -1 : stack.peek();
+                int len = i - k - 1;
+                maxArea = Math.max(maxArea, len * heights[j]);
             }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            int k = stack.isEmpty() ? -1 : stack.peek();
+            int len = heights.length - k - 1;
+            maxArea = Math.max(maxArea, len * heights[j]);
         }
         return maxArea;
     }
@@ -27,4 +47,5 @@ public class Solution {
         int[] heights = {2, 1, 5, 6, 2, 3};
         System.out.println(solution.largestRectangleArea(heights));
     }
+
 }
