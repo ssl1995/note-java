6b281a33687d10db75d53d120c7d408eb311d6c0
songshenglin <872912679@qq.com>
2022-11-12 19:43:39 +0800

    和最小的k个数对

--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第9章_堆/q61_和最小的k个数对/Solution.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第9章_堆/q61_和最小的k个数对/Solution.java
@@ -1,9 +1,9 @@
 package com.ssl.note.algorithm.leetcode.剑指Offer.第三版.第9章_堆.q61_和最小的k个数对;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * @author SongShengLin
@@ -12,29 +12,49 @@ import java.util.PriorityQueue;
  */
 public class Solution {
 
+    /**
+     * 和最小的k个数对
+     * 方法1：时间复杂度O(k^2 * logK)
+     */
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        // 最大堆：存k个和最小的数对
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((arr1, arr2) -> arr2[0] + arr2[1] - (arr1[0] + arr1[1]));
-        // 优化：由于nums1和nums2都是递增排序的，较小的数对和只会存在前k个
-        for (int i = 0; i < Math.min(nums1.length, k); i++) {
-            for (int j = 0; j < Math.min(nums2.length, k); j++) {
+        if (nums1 == null || nums2 == null || k < 0) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        // 最大堆
+        Queue<int[]> maxHeap = new PriorityQueue<>((n1, n2) -> n2[0] + n2[1] - (n1[0] + n1[1]));
+        // 优化:nums都是排序的，所以我们只用比对前k个即可
+        for (int i = 0; i < Math.min(k, nums1.length); i++) {
+            for (int j = 0; j < Math.min(k, nums2.length); j++) {
+                int num1 = nums1[i];
+                int num2 = nums2[j];
                 if (maxHeap.size() < k) {
-                    maxHeap.offer(new int[]{nums1[i], nums2[j]});
+                    maxHeap.offer(new int[]{num1, num2});
                 } else {
-                    int[] temp = maxHeap.peek();
-                    // 如果堆顶数对和>待插入的数对和，堆顶移除
-                    if (temp[0] + temp[1] > nums1[i] + nums2[j]) {
+                    int[] peek = maxHeap.peek();
+                    int peekSum = peek[0] + peek[1];
+                    if (num1 + num2 < peekSum) {
                         maxHeap.poll();
-                        maxHeap.offer(new int[]{nums1[i], nums2[j]});
+                        maxHeap.offer(new int[]{num1, num2});
                     }
                 }
             }
         }
-        List<List<Integer>> res = new ArrayList<>();
-        while (!maxHeap.isEmpty()) {
-            int[] temp = maxHeap.poll();
-            res.add(Arrays.asList(temp[0], temp[1]));
+
+        for (int[] nums : maxHeap) {
+            List<Integer> sub = new ArrayList<>();
+            sub.add(nums[0]);
+            sub.add(nums[1]);
+            res.add(sub);
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {1, 5, 13, 21};
+        int[] nums2 = {2, 4, 9, 15};
+        int k = 3;
+        System.out.println(solution.kSmallestPairs(nums1, nums2, k));
     }
 }
--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第9章_堆/q61_和最小的k个数对/Solution1.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第9章_堆/q61_和最小的k个数对/Solution1.java
@@ -9,25 +9,41 @@ import java.util.*;
  */
 public class Solution1 {
 
+    /**
+     * 和最小的k个数对
+     * 方法2：时间复杂度O(k * logK)，相比方法1降低了k次时间复杂度
+     */
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        // 最小堆：int[]是一个从长度为2的数组，第1个数字表示数对中第1个数在nums1中的下标；第2个数字表示数对中第2个数在nums2中的下标
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(p -> nums1[p[0]] + nums2[p[1]]));
+        if (nums1 == null || nums2 == null || k < 0) {
+            return new ArrayList<>();
+        }
+        // 最小值堆：存两个数组的下标
+        Queue<int[]> minIndexHeap = new PriorityQueue<>(Comparator.comparing(index -> nums1[index[0]] + nums2[index[1]]));
         if (nums2.length > 0) {
-            // 优化：数组都是递增的，取前k个即可
             for (int i = 0; i < Math.min(k, nums1.length); i++) {
-                // 数对中第1个数是nums1中取前k个，第2个数先只存0
-                minHeap.offer(new int[]{i, 0});
+                minIndexHeap.offer(new int[]{i, 0});
             }
         }
         List<List<Integer>> res = new ArrayList<>();
-        while (k-- > 0 && !minHeap.isEmpty()) {
-            int[] index = minHeap.poll();
+
+        while (!minIndexHeap.isEmpty() && k-- > 0) {
+            // 优化：由于数组是递增有序的，从小达大出栈的都是要入结果集的最小值对，这里就不需要再peek，直接poll就行
+            int[] index = minIndexHeap.poll();
             res.add(Arrays.asList(nums1[index[0]], nums2[index[1]]));
 
             if (index[1] < nums2.length - 1) {
-                minHeap.add(new int[]{index[0], index[1] + 1});
+                minIndexHeap.offer(new int[]{index[0], index[1] + 1});
             }
         }
+
         return res;
+    }
+
+    public static void main(String[] args) {
+        Solution1 solution = new Solution1();
+        int[] nums1 = {1, 5, 13, 21};
+        int[] nums2 = {2, 4, 9, 15};
+        int k = 3;
+        System.out.println(solution.kSmallestPairs(nums1, nums2, k));
     }
 }
