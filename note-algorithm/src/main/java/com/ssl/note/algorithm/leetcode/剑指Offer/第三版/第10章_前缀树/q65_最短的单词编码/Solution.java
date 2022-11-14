4f141906e4b11b7bc01c37fc3dcad59bedfda5af
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-11-14 11:43:02 +0800

    最短单词编码

--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第10章_前缀树/q65_最短的单词编码/Solution.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/剑指Offer/第三版/第10章_前缀树/q65_最短的单词编码/Solution.java
@@ -7,49 +7,76 @@ package com.ssl.note.algorithm.leetcode.剑指Offer.第三版.第10章_前缀树
  */
 public class Solution {
 
-    static class TrieNode {
-        public TrieNode[] children;
+    private int count;
 
-        public TrieNode() {
-            children = new TrieNode[26];
+    /**
+     * 最短单词编码
+     * 输入：words = ["time", "me", "bell"]
+     * 输出：10
+     * 解释：一组有效编码为 s = "time#bell#" 和 indices = [0, 2, 5] 。
+     * words[0] = "time" ，s 开始于 indices[0] = 0 到下一个 '#' 结束的子字符串，如加粗部分所示 "time#bell#"
+     * words[1] = "me" ，s 开始于 indices[1] = 2 到下一个 '#' 结束的子字符串，如加粗部分所示 "time#bell#"
+     * words[2] = "bell" ，s 开始于 indices[2] = 5 到下一个 '#' 结束的子字符串，如加粗部分所示 "time#bell#"
+     */
+    public int minimumLengthEncoding(String[] words) {
+        if (words == null || words.length == 0) {
+            return 0;
+        }
+        TrieNode root = build(words);
+
+        dfs(root, 1);
+        return count;
+    }
+
+    private void dfs(TrieNode node, int len) {
+        boolean isLeaf = true;
+
+        for (TrieNode next : node.nexts) {
+            if (next != null) {
+                isLeaf = false;
+                dfs(next, len + 1);
+            }
+        }
+
+        if (isLeaf) {
+            count += len;
         }
     }
 
-
-    public int minimumLengthEncoding(String[] words) {
-        TrieNode root = buildTrie(words);
-
-        int[] total = {0};
-        dfs(root, 1, total);
-        return total[0];
-    }
-
-    private TrieNode buildTrie(String[] words) {
+    private TrieNode build(String[] words) {
+        if (words == null || words.length == 0) {
+            return new TrieNode();
+        }
+        // 记录根节点
         TrieNode root = new TrieNode();
+
         for (String word : words) {
-            TrieNode cur = root;
-            // 倒序遍历word，word是后缀，倒序后就是前缀啦
-            for (int i = word.length() - 1; i >= 0; i--) {
-                char c = word.charAt(i);
-                if (cur.children[c - 'a'] == null) {
-                    cur.children[c - 'a'] = new TrieNode();
+            TrieNode node = root;
+
+            char[] cs = word.toCharArray();
+            // 从后往前，形成后缀字符串的前缀树
+            for (int j = cs.length - 1; j >= 0; j--) {
+                char c = cs[j];
+                if (node.nexts[c - 'a'] == null) {
+                    node.nexts[c - 'a'] = new TrieNode();
                 }
-                cur = cur.children[c - 'a'];
+                node = node.nexts[c - 'a'];
             }
         }
         return root;
     }
 
-    private void dfs(TrieNode root, int length, int[] total) {
-        boolean isLeaf = true;
-        for (TrieNode child : root.children) {
-            if (child != null) {
-                isLeaf = false;
-                dfs(child, length + 1, total);
-            }
+    class TrieNode {
+        private TrieNode[] nexts;
+
+        public TrieNode() {
+            nexts = new TrieNode[26];
         }
-        if (isLeaf) {
-            total[0] += length;
-        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] words = {"time", "me", "bell"};
+        System.out.println(solution.minimumLengthEncoding(words));
     }
 }
