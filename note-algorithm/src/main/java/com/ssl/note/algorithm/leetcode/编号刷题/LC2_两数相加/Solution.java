56fafc446ba64f4c0eda4fff10a22ff1621ca521
songshenglin_i <872912679@qq.com>
2022-12-26 22:35:43 +0800

    LC2_两数相加

--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/编号刷题/LC2_两数相加/Solution.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/编号刷题/LC2_两数相加/Solution.java
@@ -11,15 +11,16 @@ public class Solution {
      * 解释：342 + 465 = 807.
      */
     public ListNode addTwoNumbers(ListNode head1, ListNode head2) {
-        // 设计链表操作，虚拟头结点是个好习惯
+        // 1.设计链表操作，虚拟头结点是个好习惯
         ListNode dummyNode = new ListNode(-1);
         ListNode cur = dummyNode;
-        // 记录进位信息=0,1的状态
+        // 2.记录进位信息=0,1的状态
         int carry = 0;
 
         while (head1 != null || head2 != null) {
-            // 这一轮加起来的数先继承上一轮的进位值
+            // 3.这一轮加起来的数先继承上一轮的进位值
             int sum = carry;
+            // 4.哪个指针为空，就哪个计算sum
             if (head1 != null) {
                 sum += head1.val;
                 head1 = head1.next;
@@ -28,19 +29,18 @@ public class Solution {
                 sum += head2.val;
                 head2 = head2.next;
             }
-            // 和算完，立马更新进位信息
+            // 5.计算下一个节点的信息
             carry = sum / 10;
-
             cur.next = new ListNode(sum % 10);
             cur = cur.next;
         }
 
-        // 边界：最低位如果还存在进位，就添加到链表末尾
+        // 6.最低位如果还存在进位，就添加到链表末尾
         if (carry == 1) {
             cur.next = new ListNode(carry);
         }
 
-        // 返回哑结点.next
+        // 7.返回哑结点.next
         return dummyNode.next;
     }
 
