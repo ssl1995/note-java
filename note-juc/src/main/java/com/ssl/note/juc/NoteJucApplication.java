abee50b38d7582bacd8d8bb206ad285b478efae8
songshenglin_i <872912679@qq.com>
2023-01-02 11:12:58 +0800

    创建Note-juc目录

---
+++ note-juc/src/main/java/com/ssl/note/juc/NoteJucApplication.java
@@ -0,0 +1,13 @@
+package com.ssl.note.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteJucApplication.class, args);
+    }
+
+}
---
+++ note-juc/src/test/java/com/ssl/note/juc/NoteJucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssl.note.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteJucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
