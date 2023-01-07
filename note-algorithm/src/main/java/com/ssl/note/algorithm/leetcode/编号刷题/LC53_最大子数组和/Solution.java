e63abd625ef5a45f06db0348ffee5cbcaa02b12f
songshenglin_i <872912679@qq.com>
2023-01-07 18:13:00 +0800

    最大子数组和

--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/编号刷题/LC53_最大子数组和/Solution.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/编号刷题/LC53_最大子数组和/Solution.java
@@ -16,29 +16,23 @@ public class Solution {
      * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
      */
     public int maxSubArray(int[] nums) {
-        if (nums.length < 2) {
-            return nums.length == 0 ? -1 : nums[0];
+        if (nums == null || nums.length == 0) {
+            return 0;
         }
-        // dp[i]:i位置目前的最大子数组和为多少
-        int[] dp = new int[nums.length];
-        // 0位置初始化
-        dp[0] = nums[0];
-        // max初始化为dp[0]防止数组只有一个元素没法遍历的情况
-        int max = dp[0];
-        // 从1位置开始遍历
-        for (int i = 1; i < nums.length; i++) {
-            // 前面的子数组和为负数，就放弃
-            if (dp[i - 1] <= 0) {
-                dp[i] = nums[i];
-            } else {
-                // 前面的子数组和>0，就加入
-                dp[i] = dp[i - 1] + nums[i];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        // max与pre比较，pre可能为负数，所以max初值为最小的负数
+        int max = Integer.MIN_VALUE;
+        int pre = 0;
+        int index = 0;
+        while (index <= nums.length - 1) {
+            if (pre < 0) {
+                pre = 0;
             }
-            // 遍历选择其中最大值
-            max = Math.max(max, dp[i]);
+            pre += nums[index++];
+            max = Math.max(max, pre);
         }
-        System.out.println("nums:" + Arrays.toString(nums));
-        System.out.println("dp:" + Arrays.toString(dp));
         return max;
     }
 
