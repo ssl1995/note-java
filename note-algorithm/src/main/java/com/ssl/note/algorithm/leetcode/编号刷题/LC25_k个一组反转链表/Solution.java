3132f78ac4cc1f8aa32aabaf2868f57e3b5a2822
songshenglin <872912679@qq.com>
2023-03-06 15:25:10 +0800

    LC25_k个一组反转链表

--- note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/编号刷题/LC25_k个一组反转链表/Solution.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/leetcode/编号刷题/LC25_k个一组反转链表/Solution.java
@@ -17,32 +17,35 @@ public class Solution {
         }
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
-        // 翻转外围的指针
+
+        // 将链表分为已反转区域、待反转区域、未反转区域
+        // 待反转区域四个指针: pre [start、end] next
         ListNode pre = dummy;
         ListNode next = null;
-        // 真正反转的内部指针
-        ListNode end = dummy;
         ListNode start = null;
+        ListNode end = dummy;
 
         while (end.next != null) {
-            // 不够k个，保持原样
+            // 确定待翻转区域的start和end
             for (int i = 0; i < k && end != null; i++) {
                 end = end.next;
             }
-            // 说明整个链表长度不够k个，直接保持原样返回
+            // 待反转区域长度不够k个，不反转
             if (end == null) {
                 break;
             }
-            // 确定外围指针、反转内部指针
+            // pre [start、end] next
             start = pre.next;
             next = end.next;
-            // 翻转开始
+
+            // 翻转开始，将[start,end]->[end,start]
             end.next = null;
             pre.next = reverse(start);
             start.next = next;
-            // 重置pre和reverseEnd
+
+            // 将pre和end重置到start位置，开启下一个遍历
             end = start;
-            pre = end;
+            pre = start;
         }
 
         return dummy.next;
