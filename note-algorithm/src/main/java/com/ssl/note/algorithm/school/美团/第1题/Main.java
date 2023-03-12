efffe04bf21a42d36e945d75183d2ea8e402ded8
ssl1995 <13639120050@163.com>
2023-03-12 08:21:22 +0800

    美团面试第一题

--- note-algorithm/src/main/java/com/ssl/note/algorithm/school/美团/第1题/Main.java
+++ note-algorithm/src/main/java/com/ssl/note/algorithm/school/美团/第1题/Main.java
@@ -1,58 +1,49 @@
 package com.ssl.note.algorithm.school.美团.第1题;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String s = scanner.next();
-            res.add(longestPalindromeLen(s));
-        }
-        for (Integer num : res) {
-            System.out.println(num);
-        }
+        String str = scanner.next();
+        System.out.println(getMinChange(str));
         scanner.close();
     }
 
     /**
-     * 最长回文串长度
+     * 输入一个数字字符串，最少让它连续的数字不重复的次数
+     * 比如：111222333，最少需要3次修改为121212313不重复
      */
-    private static int longestPalindromeLen(String s) {
-        if (s == null || s.length() == 0) {
+    private static int getMinChange(String str) {
+        if (Objects.isNull(str)) {
             return 0;
         }
-        char[] str = s.toCharArray();
-        int N = str.length;
-        int[][] dp = new int[N][N];
-        // 初始化
-        // 对角线后面那根线末尾位置，没有R-1，所以先填上
-        dp[N - 1][N - 1] = 1;
-        // 到N-1停止
-        for (int i = 0; i < N - 1; i++) {
-            // 对角线
-            dp[i][i] = 1;
-            // 对角线后面那根线
-            dp[i][i + 1] = str[i] == str[i + 1] ? 2 : 1;
+        int n = str.length();
+        if (n == 1) {
+            return 0;
         }
-        // (L,R)需要左下角(L+1,R-1)，左边(L,R-1),下边(L+1,R)三个位置
-        // 所以从右下角开始，从左往右遍历
-        for (int L = N - 3; L >= 0; L--) {
-            // R从对角线后面的第二根线就开始往后遍历
-            for (int R = L + 2; R < N; R++) {
-                // 暴力递归还可以优化
-                // 可能性p1 = dp[L+1][R-1]可能省掉，因为p2,p3意见算过p1
-                dp[L][R] = Math.max(dp[L][R - 1], dp[L + 1][R]);
-                if (str[L] == str[R]) {
-                    dp[L][R] = Math.max(dp[L][R], 2 + dp[L + 1][R - 1]);
-                }
+        if (n == 2) {
+            return str.charAt(0) == str.charAt(1) ? 1 : 0;
+        }
+
+        char[] cs = str.toCharArray();
+        int[] dp = new int[n];
+        if (cs[0] == cs[1]) {
+            dp[1] = 1;
+            cs[1] = '#';
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (cs[i] == cs[i - 1]) {
+                dp[i] = dp[i - 1] + 1;
+                cs[i] = '#';
+            } else {
+                dp[i] = dp[i - 1];
             }
         }
-        return dp[0][N - 1];
+
+        return dp[n - 1];
     }
 }
